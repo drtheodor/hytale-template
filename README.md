b7a001e58af170762d695205998e1cb42bb17363
Theo <git@theo.is-a.dev>
2026-01-14 23:55:44 +0300

    refactor: use groovy dsl

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # Hytale Mod/Plugin Template
 
 Features:
-- Gradle
-- Custom setup and run tasks
-- (unfinished for now) CI
+- Gradle.
+- Custom setup and run tasks.
+- (unfinished for now) CI.
 
 ## Instructions
 1. Fork this repository.
 2. Open it in your IDE of choice.
-3. On sync follow instructions in the terminal (you'll need to log into your account to allow `hytale-downloader` to download server files).
-4. Done.
+3. Build the project.
+4. On sync follow instructions in the terminal (you'll need to log into your account to allow `hytale-downloader` to download server files).
+5. Voila!
--- flake.nix
+++ flake.nix
@@ -19,32 +19,12 @@
         nativeBuildInputs = with pkgs; [];
         buildInputs = with pkgs; [
           java
-          unzip
         ];
       in {
         devShells.default = pkgs.mkShell {
           inherit nativeBuildInputs buildInputs;
 
-          shellHook = ''
-            mkdir -p build/hytale
-            if [ ! -f build/hytale/HytaleServer.jar ] || [ ! -f build/hytale/Assets.zip ]; then
-              if [ -f ../HytaleServer.jar ] || [ -f ../Assets.zip ]; then
-                ln -sf $(readlink -f ../HytaleServer.jar) build/hytale/HytaleServer.jar
-                ln -sf $(readlink -f ../Assets.zip) build/hytale/Assets.zip
-              else
-                if [ ! -f build/hytale/hytale-downloader-linux-amd64 ]; then
-                  curl --create-dirs -O --output-dir build/tmp/ https://downloader.hytale.com/hytale-downloader.zip
-                  unzip build/tmp/hytale-downloader.zip -d build/hytale
-                  rm build/tmp/hytale-downloader.zip
-                fi
-
-                ./build/hytale/hytale-downloader-linux-amd64
-              fi
-            fi
-          '';
-
           env = {
-            LD_LIBRARY_PATH = nixpkgs.lib.makeLibraryPath buildInputs;
             JAVA_HOME = "${java.home}";
           };
         };
